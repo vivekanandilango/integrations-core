57f3b5090a9379f9febacfb7d635d7491a6849b1
Boris Kozlov <boris.kozlov@datadoghq.com>
2024-06-17 08:32:31 +0000

    proff that test is flacky by design

--- sqlserver/tests/test_statements.py
+++ sqlserver/tests/test_statements.py
@@ -302,7 +302,7 @@ def test_statement_metrics_and_plans(
     caplog.set_level(logging.INFO)
     if disable_secondary_tags:
         dbm_instance['query_metrics']['disable_secondary_tags'] = True
-    dbm_instance['query_activity'] = {'enabled': True, 'collection_interval': 2}
+    dbm_instance['query_activity'] = {'enabled': True, 'collection_interval': 0.00001}
     check = SQLServer(CHECK_NAME, {}, [dbm_instance])
 
     # the check must be run three times:
@@ -311,12 +311,12 @@ def test_statement_metrics_and_plans(
     # 2) load the test queries into the StatementMetrics state
     # 3) emit the query metrics based on the diff of current and last state
     dd_run_check(check)
-    time.sleep(2)
+    #time.sleep(2)
     for _ in range(0, exe_count):
         for params in param_groups:
             bob_conn.execute_with_retries(query, params, database=database)
     dd_run_check(check)
-    time.sleep(2)
+    #time.sleep(2)
     aggregator.reset()
     for _ in range(0, exe_count):
         for params in param_groups:
